52f6d317b6454fdf3282813f9614fe97b0bbe62e
Sacha <alexander.lansky@gmail.com>
2021-03-16 13:24:49 -0400

    Switch globe to center

--- src/components/sections/Map.js
+++ src/components/sections/Map.js
@@ -2,11 +2,9 @@ import React from "react";
 import MapChart from '.././react-simple-maps-master/MeetupMap/MapChart'
 const Map = () => {
     return (
-        <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="10px" data-reveal-delay="800">
             <div className="reveal-from-bottom" data-reveal-delay="1000">
-                <MapChart />
+                <MapChart />        
             </div>
-        </div>
     );
 };
 
